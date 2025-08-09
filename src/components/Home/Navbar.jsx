73a7ec8aa88f3a07756514e87f1cfc5e54cb22ee
tonmoy-free <tonmoykhan.free@gmail.com>
2025-08-09 19:57:09 +0600

    about us contact us name add in navbar

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -41,6 +41,8 @@ const Navbar = () => {
             <li className='text-primary dark:text-white'><NavLink to="/">Home</NavLink></li>
             <li className='text-primary dark:text-white'><NavLink to="/allArtifacts">All Artifacts</NavLink></li>
             <li className='text-primary dark:text-white'><NavLink to="/addArtifacts">Add Artifacts</NavLink></li>
+            <li className='text-primary dark:text-white'><NavLink to="/addArtifacts">About Us</NavLink></li>
+            <li className='text-primary dark:text-white'><NavLink to="/addArtifacts">Contact Us</NavLink></li>
         </>
     )
     return (
